a5ef7a0c59a299f08c46c2c4af1b592aefbffd87
Sharay Brown <SharayAntoinette@Sharays-MacBook-Pro.local>
2016-05-26 10:49:22 -0700

    Team Alpha To Do List
    Initial commit of calendar with to do list functionality

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include_once('functions.php'); ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Team Alpha To Do List</title>
+<link href='https://fonts.googleapis.com/css?family=Spinnaker' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Reenie+Beanie' rel='stylesheet' type='text/css'>
+<link type="text/css" rel="stylesheet" href="style.css">
+<script src="jquery.min.js"></script>
+</head>
+<body>
+
+<div id="calendar_div">
+	<?php echo getCalender(); ?>
+</div>
+
+</body>
+</html>
